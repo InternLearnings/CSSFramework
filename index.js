430213aa469716714dfb75b575b10ac2d9f27f5c
shruthipachava332@gmail.com <DESKTOP-RFRDR4V>
2022-01-06 16:12:29 +0530

    added progress bars and sliders

--- index.js
+++ index.js
@@ -59,3 +59,21 @@ $(document).ready(function(){
       }
   });
 });
+
+
+
+function sliderval() {
+    const myslider = document.getElementById("cm-my-slider");
+const svalue = document.getElementById("cm-slider-value");
+    svalue.textContent = myslider.value;
+}
+function slidersquareval(){
+    const myslide = document.getElementById("cm-slider-square");
+    const slidervalue = document.getElementById("cm-slider-square-value");
+    slidervalue.textContent = myslide.value;
+}
+function sliderrectangleval(){
+    const myslide = document.getElementById("cm-slider-rectangle");
+    const slidervalue = document.getElementById("cm-slider-rectangle-value");
+    slidervalue.textContent = myslide.value;
+}
